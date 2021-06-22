63393ae22aa03ba0077a9adc873b3a2de9bdf2ed
mvaldes <mvaldes@student.42.fr>
2021-06-22 19:16:29 +0200

    test loop

--- srcs/minishell.h
+++ srcs/minishell.h
@@ -1,31 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mvaldes <mvaldes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/22 18:03:21 by mvaldes           #+#    #+#             */
-/*   Updated: 2021/06/22 19:16:02 by mvaldes          ###   ########.fr       */
+/*   Created: 2021/06/22 19:11:08 by mvaldes           #+#    #+#             */
+/*   Updated: 2021/06/22 19:15:30 by mvaldes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
-#include <stdio.h>
-#include "minishell.h"
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include "../lib/get_next_line/get_next_line.h"
 
-int	main(int argc, char **argv, char **env)
-{
-	char	*line;
+# define STDIN 0
+# define STDOUT 1
+# define STDERR 2
 
-	(void)argc;
-	(void)argv;
-	(void)env;
-	line = NULL;
-	printf(">$\n");
-	while (get_next_line(STDIN, &line) > 0)
-	{
-		printf(">$\n");
-	}
-	return (1);
-}
+#endif
