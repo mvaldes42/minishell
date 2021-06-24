9bbedf5adb86c47dc88a27123eb46fc4566ce642
mvaldes <mvaldes@student.42.fr>
2021-06-24 17:04:25 +0200

    working prompt

--- srcs/main.c
+++ srcs/main.c
@@ -6,26 +6,44 @@
 /*   By: mvaldes <mvaldes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 18:03:21 by mvaldes           #+#    #+#             */
-/*   Updated: 2021/06/22 19:28:45 by mvaldes          ###   ########.fr       */
+/*   Updated: 2021/06/24 17:03:55 by mvaldes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void print_prompt(void)
+{
+	char	cwd[256];
+
+	ft_putstr_fd("-> ", STDOUT);
+	ft_putstr_fd(getcwd(cwd, sizeof(cwd)), STDOUT);
+	ft_putstr_fd(" >$ ", STDOUT);
+}
+
+int	lexer(char *line, t_data *data)
+{
+	(void)line;
+	(void)data;
+	return (1);
+}
+
 int	main(int argc, char **argv, char **env)
 {
-	char	*line;
-	char	cwd[256];
+	char			*line;
+	t_data			data;
 
 	(void)argc;
 	(void)argv;
 	(void)env;
 	line = NULL;
-	printf("%s >$ ", getcwd(cwd, sizeof(cwd)));
+	// data.s_tokens->tokens_lst->token_type = CHAR_SINGLE_QUOTE;
+	// printf("%c\n", data.s_tokens->tokens_lst->token_type);
+	print_prompt();
 	while (get_next_line(STDIN, &line) > 0)
 	{
-	// 	printf("line : %s\n", line);
-	// 	printf("%s >$ ", getcwd(cwd, sizeof(cwd)));
+		lexer(line, &data);
+		print_prompt();
 	}
 	return (1);
 }
--- srcs/minishell.h
+++ srcs/minishell.h
@@ -6,18 +6,55 @@
 /*   By: mvaldes <mvaldes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 19:11:08 by mvaldes           #+#    #+#             */
-/*   Updated: 2021/06/22 19:17:13 by mvaldes          ###   ########.fr       */
+/*   Updated: 2021/06/24 16:58:32 by mvaldes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 # include "../lib/get_next_line/get_next_line.h"
+# include "../lib/libft/libft.h"
 # include <stddef.h>
 # include <stdio.h>
+# include <string.h>
 
 # define STDIN 0
 # define STDOUT 1
 # define STDERR 2
+
+const char	*g_build_in[] =
+{"echo", "cd", "pwd", "export", "unset", "env", "exit"};
+
+typedef enum e_token_type_lst
+{
+	CHAR_SINGLE_QUOTE = '\'',
+	CHAR_DOUBLE_QUOTE = '\"',
+	CHAR_GREATER = '>',
+	CHAR_LESSER = '<',
+	CHAR_PIPE = '|',
+	CHAR_WHITESPACE = ' ',
+	CHAR_TAB = '\t',
+	CHAR_NEWLINE = '\n',
+	CHAR_NULL = 0,
+	CHAR_EXPANSION = '$',
+	CHAR_SINGLE_WILD = '?',
+}	t_token_type;
+
+typedef struct s_token_id
+{
+	int		token_type;
+	char			*token_ptr;
+}	t_token_id;
+
+typedef struct s_lexer
+{
+	t_token_id	*tokens_lst;
+	long		token_nbr;
+}	t_lexer;
+
+typedef struct s_data
+{
+	t_lexer	*s_tokens;
+}	t_data;
 
 #endif
