b09ee1e9aa4b3b7ab3dae01b0a530b623911caf8
mvaldes <mvaldes@student.42.fr>
2021-07-24 19:07:03 +0200

    g_utils change

--- srcs/utils/general_utils.h
+++ srcs/utils/general_utils.h
@@ -2,14 +2,26 @@
 # define GENERAL_UTILS_H
 # include "../minishell.h"
 
+typedef struct s_split
+{
+	char		**dest;
+	char		c;
+	int			start;
+	int			end;
+	int			i;
+	int			w_count;
+}	t_split;
+
 void	exit_fail(t_data *data);
 void	exit_sucess(t_data *data, char *line);
 void	clear_data(t_data *data);
 void	create_prompt(t_data *data);
 
-int		sep_token_count(t_data *data, char *str, char *sep);
 int		char_occu(char *str, char c);
-char	*ft_strtok(char *s, const char *delim);
-char	**ft_strtok_spec(t_data *data, char *s, int tk_nbr);
+
+char	**token_split(char const *s, char c);
+int		token_count(const char *s, char c);
 
 #endif
+
+//  echo -n bonjour|echo cool' $HOME top '" $HOME    super "|echo $?>txt1
