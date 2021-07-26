674ef9b810ef124a569feb0298b5050e856b442b
Cyrena RAMDANI <cramdani@42.student.fr>
2021-07-26 18:41:48 +0200

    start execution

---
+++ srcs/executor/exec.h
@@ -0,0 +1,30 @@
+#ifndef EXEC_H
+#define EXEC_H
+
+# include "../../lib/libft/libft.h"
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <string.h>
+# include <stddef.h>
+# include <stdbool.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include <sys/stat.h>
+# include <dirent.h>
+# include <sys/ioctl.h>
+# include <stdlib.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h>
+
+# define STDIN 0
+# define STDOUT 1
+# define STDERR 2
+
+# define TRUE 1
+# define FALSE 0
+
+#endif
