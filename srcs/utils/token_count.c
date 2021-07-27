9dfd1d40c522bbb0084c4d0cf78019b61abf0abb
mvaldes <mvaldes@student.42.fr>
2021-07-27 12:10:40 +0200

    error cases

--- srcs/utils/token_count.c
+++ srcs/utils/token_count.c
@@ -6,7 +6,7 @@
 /*   By: mvaldes <mvaldes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/24 16:09:08 by mvaldes           #+#    #+#             */
-/*   Updated: 2021/07/24 18:44:52 by mvaldes          ###   ########.fr       */
+/*   Updated: 2021/07/27 12:09:40 by mvaldes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,85 +14,92 @@
 
 static int	word_case_count(char **dup, char c, int count)
 {
-	while (**dup && **dup != c && **dup != '\'' && **dup != '\"' && \
-	**dup != '|' && **dup != '<' && **dup != '>' && **dup != '$')
+	while (**dup && **dup != c && **dup != S_QUOTE && **dup != D_QUOTE && **dup \
+	!= PIPE_C && **dup != R_IN && **dup != R_OUT && **dup != VAR)
 		*dup += 1;
 	count++;
 	return (count);
 }
 
-static int	quotes_case_count(char **dup, int count)
+static int	quotes_case_general(char **dup, int count, char q_type)
 {
-	if (**dup && **dup == '\'')
-	{
-		*dup += 1;
-		while (**dup && **dup != '\'')
-			*dup += 1;
-		count++;
+	*dup += 1;
+	while (**dup != q_type)
+	{	
+		if (**dup == '\0')
+		{
+			printf("error: missing quote\n");
+			return (0);
+		}
 		*dup += 1;
 	}
-	else if (**dup && **dup == '\"')
+	if (**dup != '\0' && count != 0)
 	{
-		*dup += 1;
-		while (**dup && **dup != '\"')
-			*dup += 1;
 		count++;
 		*dup += 1;
 	}
 	return (count);
 }
 
+static int	quotes_case_count(char **dup, int count)
+{
+	if (**dup && **dup == S_QUOTE)
+		count = quotes_case_general(dup, count, S_QUOTE);
+	else if (**dup && **dup == D_QUOTE)
+		count = quotes_case_general(dup, count, D_QUOTE);
+	return (count);
+}
+
 static int	spe_case_count(char **dup, char c, int count)
 {
-	if (**dup == '|')
+	if (**dup == PIPE_C)
 	{
 		count++;
 		*dup += 1;
 	}
-	else if (**dup == '$')
+	else if (**dup == VAR)
 	{
 		*dup += 1;
 		if (**dup == '?')
 			*dup += 1;
 		else
 		{
-			while (**dup && **dup != c && **dup != '\'' && **dup != '\"' && \
-			**dup != '|' && **dup != '<' && **dup != '>' && **dup != '$' && \
-			**dup != '?')
+			while (**dup && **dup != c && **dup != S_QUOTE && **dup != D_QUOTE \
+			&& **dup != PIPE_C && **dup != R_IN && **dup != R_OUT && **dup != \
+			VAR && **dup != '?')
 				*dup += 1;
 		}
 		count += 1;
 	}
 	return (count);
 }
-// count = 0 have to be replaced with an erro message
+
+static int	redir_case_general(char **dup, int count, char r_type, char other_r)
+{
+	*dup += 1;
+	if (**dup == other_r)
+	{
+		printf("error: near redirection symbol\n");
+		return (0);
+	}
+	else if (**dup == r_type)
+		*dup += 1;
+	if (**dup == r_type || **dup == other_r)
+	{
+		printf("error: near redirection symbol\n");
+		return (0);
+	}
+	else
+		count += 1;
+	return (count);
+}
 
 static int	redir_case_count(char **dup, int count)
 {
-	if (**dup == '<')
-	{
-		*dup += 1;
-		if (**dup == '>')
-			count = 0;
-		else if (**dup == '<')
-			*dup += 1;
-		if (**dup == '<' || **dup == '>')
-			count = 0;
-		else
-			count += 1;
-	}
-	else if (**dup == '>')
-	{
-		*dup += 1;
-		if (**dup == '<')
-			count = 0;
-		else if (**dup == '>')
-			*dup += 1;
-		if (**dup == '<' || **dup == '>')
-			count = 0;
-		else
-			count += 1;
-	}
+	if (**dup == R_IN)
+		count = redir_case_general(dup, count, R_IN, R_OUT);
+	else if (**dup == R_OUT)
+		count = redir_case_general(dup, count, R_OUT, R_IN);
 	return (count);
 }
 
@@ -109,18 +116,22 @@ int	token_count(const char *s, char c)
 	{
 		while (*dup && *dup == c)
 			dup++;
-		if (*dup && *dup != c && *dup != '\'' && *dup != '\"' && *dup != '|'\
-		&& *dup != '<' && *dup != '>' && *dup != '$')
+		if (*dup && *dup != c && *dup != S_QUOTE && *dup != D_QUOTE && \
+		*dup != PIPE_C && *dup != R_IN && *dup != R_OUT && *dup != VAR)
 			count = word_case_count(&dup, c, count);
-		if (*dup && (*dup == '\'' || *dup == '\"'))
+		if (*dup && (*dup == S_QUOTE || *dup == D_QUOTE))
+		{
 			count = quotes_case_count(&dup, count);
-		else if (*dup && (*dup == '|' || *dup == '$'))
+			if (count == 0)
+				return (count);
+		}
+		else if (*dup && (*dup == PIPE_C || *dup == VAR))
 			count = spe_case_count(&dup, c, count);
-		else if (*dup && (*dup == '<' || *dup == '>'))
+		else if (*dup && (*dup == R_IN || *dup == R_OUT))
 		{
 			count = redir_case_count(&dup, count);
 			if (count == 0)
-				break ;
+				return (count);
 		}
 	}
 	free(ptr);
--- srcs/utils/token_split.c
+++ srcs/utils/token_split.c
@@ -6,7 +6,7 @@
 /*   By: mvaldes <mvaldes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/24 15:23:03 by mvaldes           #+#    #+#             */
-/*   Updated: 2021/07/26 17:11:35 by mvaldes          ###   ########.fr       */
+/*   Updated: 2021/07/27 11:59:51 by mvaldes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 static void	regular_word(t_split *s, const char *str)
 {
-	while (str[s->end] && str[s->end] != s->c && str[s->end] != '\'' && \
-	str[s->end] != '\"' && str[s->end] != '|' && str[s->end] != '<' && \
-	str[s->end] != '>' && str[s->end] != '$')
+	while (str[s->end] && str[s->end] != s->c && str[s->end] != S_QUOTE && \
+	str[s->end] != D_QUOTE && str[s->end] != PIPE_C && str[s->end] != R_IN && \
+	str[s->end] != R_OUT && str[s->end] != VAR)
 		s->end++;
 	s->dest[s->i] = ft_substr(str, s->start, (s->end - s->start));
 	s->start = s->end;
@@ -25,20 +25,20 @@ static void	regular_word(t_split *s, const char *str)
 
 static void	quote_word(t_split *s, const char *str)
 {
-	if (str[s->end] == '\'')
+	if (str[s->end] == S_QUOTE)
 	{
 		s->end += 1;
-		while (str[s->end] && str[s->end] != '\'')
+		while (str[s->end] && str[s->end] != S_QUOTE)
 			s->end++;
 		s->end += 1;
 		s->dest[s->i] = ft_substr(str, s->start, (s->end - s->start));
 		s->start = s->end;
 		s->i++;
 	}
-	else if (str[s->end] == '\"')
+	else if (str[s->end] == D_QUOTE)
 	{
 		s->end += 1;
-		while (str[s->end] && str[s->end] != '\"')
+		while (str[s->end] && str[s->end] != D_QUOTE)
 			s->end++;
 		s->end += 1;
 		s->dest[s->i] = ft_substr(str, s->start, (s->end - s->start));
@@ -49,7 +49,7 @@ static void	quote_word(t_split *s, const char *str)
 
 static void	spe_word(t_split *s, const char *str)
 {
-	if (str[s->end] == '|')
+	if (str[s->end] == PIPE_C)
 	{
 		s->dest[s->i] = malloc(sizeof(char) * 2);
 		s->dest[s->i][0] = str[s->end];
@@ -58,16 +58,16 @@ static void	spe_word(t_split *s, const char *str)
 		s->start = s->end;
 		s->i++;
 	}
-	else if (str[s->end] == '$')
+	else if (str[s->end] == VAR)
 	{
 		s->end += 1;
 		if (str[s->end] == '?')
 			s->end += 1;
 		else
 		{
-			while (str[s->end] && str[s->end] != s->c && str[s->end] != '\'' && \
-			str[s->end] != '\"' && str[s->end] != '|' && str[s->end] != '<' && \
-			str[s->end] != '>' && str[s->end] != '$')
+			while (str[s->end] && str[s->end] != s->c && str[s->end] != S_QUOTE \
+			&& str[s->end] != D_QUOTE && str[s->end] != PIPE_C && str[s->end] \
+			!= R_IN && str[s->end] != R_OUT && str[s->end] != VAR)
 				s->end++;
 		}
 		s->dest[s->i] = ft_substr(str, s->start, (s->end - s->start));
@@ -79,7 +79,7 @@ static void	spe_word(t_split *s, const char *str)
 static void	redir_word(t_split *s, const char *str)
 {
 	s->end += 1;
-	if (str[s->end] == '>' || str[s->end] == '<')
+	if (str[s->end] == R_OUT || str[s->end] == R_IN)
 		s->end += 1;
 	s->dest[s->i] = ft_substr(str, s->start, (s->end - s->start));
 	s->start = s->end;
@@ -95,15 +95,15 @@ static void	special_split_2(t_split *s, const char *str)
 		if (!str[s->start])
 			break ;
 		s->end = s->start;
-		if (str[s->end] && str[s->end] != s->c && str[s->end] != '\'' && \
-		str[s->end] != '\"' && str[s->end] != '|' && str[s->end] != '<' && \
-		str[s->end] != '>' && str[s->end] != '$')
+		if (str[s->end] && str[s->end] != s->c && str[s->end] != S_QUOTE && \
+		str[s->end] != D_QUOTE && str[s->end] != PIPE_C && str[s->end] != \
+		R_IN && str[s->end] != R_OUT && str[s->end] != VAR)
 			regular_word(s, str);
-		if (str[s->end] && (str[s->end] == '\'' || str[s->end] == '\"'))
+		if (str[s->end] && (str[s->end] == S_QUOTE || str[s->end] == D_QUOTE))
 			quote_word(s, str);
-		else if (str[s->end] && (str[s->end] == '|' || str[s->end] == '$'))
+		else if (str[s->end] && (str[s->end] == PIPE_C || str[s->end] == VAR))
 			spe_word(s, str);
-		else if (str[s->end] && (str[s->end] == '<' || str[s->end] == '>'))
+		else if (str[s->end] && (str[s->end] == R_IN || str[s->end] == R_OUT))
 			redir_word(s, str);
 	}
 }
@@ -117,6 +117,8 @@ char	**token_split(char const *str, char c)
 	ft_memset(&s_data, 0, sizeof(t_split));
 	s_data.c = c;
 	s_data.w_count = token_count(str, c);
+	if (s_data.w_count == 0)
+		return (NULL);
 	s_data.dest = (char **)malloc(sizeof(char *) * (s_data.w_count + 1));
 	if (!s_data.dest)
 		return (NULL);
