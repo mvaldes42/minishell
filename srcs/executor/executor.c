91ca8ac74be567b13328f7c4bf31e6bcf0211c4f
Cyrena RAMDANI <cramdani@42.student.fr>
2021-07-28 18:47:35 +0200

    header and env

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -1,19 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   executor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cramdani <cramdani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/26 17:50:41 by cramdani          #+#    #+#             */
-/*   Updated: 2021/07/26 18:25:29 by cramdani         ###   ########.fr       */
+/*   Updated: 2021/07/28 18:32:56 by cramdani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
-#include <stdlib.h>
-int main()
+
+int main(int ac, char **av, char **env)
 {
-	if (fork()==0)
-		execv("/bin/ls", "");
+	(void)ac;
+	(void)av;
+	int i = 0;
+	// execv("/bin/ls", "");
+	while (env[i + 1] != NULL)
+	{
+		printf("%s\n", env[i]);
+		i++;
+	}
 }
