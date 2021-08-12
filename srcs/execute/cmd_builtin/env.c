d84237ef418debfed85eed2c0ff491bf46e973c4
mvaldes <mvaldes@student.42.fr>
2021-08-12 15:15:08 +0200

    env done

--- srcs/execute/cmd_builtin/env.c
+++ srcs/execute/cmd_builtin/env.c
@@ -6,14 +6,24 @@
 /*   By: mvaldes <mvaldes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 18:42:44 by mvaldes           #+#    #+#             */
-/*   Updated: 2021/08/12 14:27:48 by mvaldes          ###   ########.fr       */
+/*   Updated: 2021/08/12 15:13:54 by mvaldes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../execute.h"
+#include <stdio.h>
 
 int	builtin_env(char **args)
 {
+	extern char	**environ;
+	int			i;
+
 	(void)args;
+	i = 0;
+	while (environ[i])
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
 	return (1);
 }
