eaff19da68c4717ad883ac21b10b4c4b893fdd9d
Florian CAVILLON <fcavillo@e1r7p18.42.fr>
2021-08-12 17:12:20 +0200

    clean

--- srcs/execute/execute.h
+++ srcs/execute/execute.h
@@ -6,45 +6,15 @@
 /*   By: fcavillo <fcavillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 18:08:59 by mvaldes           #+#    #+#             */
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-/*   Updated: 2021/08/12 16:38:19 by fcavillo         ###   ########.fr       */
-=======
-=======
->>>>>>> pwd changed args
-<<<<<<< HEAD
-/*   Updated: 2021/08/12 14:25:30 by mvaldes          ###   ########.fr       */
-=======
-/*   Updated: 2021/08/12 12:36:30 by fcavillo         ###   ########.fr       */
->>>>>>> pwd
-<<<<<<< HEAD
->>>>>>> pwd
-=======
-=======
-/*   Updated: 2021/08/12 14:24:27 by fcavillo         ###   ########.fr       */
->>>>>>> pwd changed args
->>>>>>> pwd changed args
-=======
-/*   Updated: 2021/08/12 16:48:34 by fcavillo         ###   ########.fr       */
->>>>>>> pwd return
+/*   Updated: 2021/08/12 17:12:01 by fcavillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXECUTE_H
 # define EXECUTE_H
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 #include "../minishell.h"
 
-=======
-<<<<<<< HEAD
->>>>>>> pwd changed args
-=======
-#include "../minishell.h"
-
->>>>>>> pwd return
 int	builtin_cd(char **args);
 int	builtin_echo(char **args);
 int	builtin_env(char **args);
