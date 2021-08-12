326da6aa1f056678d9016847f43dafeac6f352e7
mvaldes <mvaldes@student.42.fr>
2021-08-12 16:27:18 +0200

    first arg is fct name

--- srcs/parsing/command_table.c
+++ srcs/parsing/command_table.c
@@ -6,7 +6,7 @@
 /*   By: mvaldes <mvaldes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 15:35:03 by mvaldes           #+#    #+#             */
-/*   Updated: 2021/08/12 14:46:15 by mvaldes          ###   ########.fr       */
+/*   Updated: 2021/08/12 16:14:42 by mvaldes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,8 +64,9 @@ static int	cmd_args(t_data *d, t_commands *cmd, t_token *tks, int i)
 {
 	int	k;
 
-	k = -1;
-	while (++k < d->pars.argv_size[cmd->id] && ++i < d->pars.tk_nbr)
+	k = 0;
+	cmd->args[k] = cmd->fct.name;
+	while (++k < d->pars.argv_size[cmd->id] + 1 && ++i < d->pars.tk_nbr)
 	{
 		if (tks[i].type == WEAK_WORD)
 			cmd->args[k] = tks[i].trans_weak;
@@ -141,7 +142,7 @@ int	input_command_table(t_data *d)
 	while (++j < d->pars.cmd_nbr && ++i < d->pars.tk_nbr)
 	{
 		memset(&cmds[j], 0, sizeof(t_commands));
-		d->cmds[j].args = malloc(sizeof(char *) * (d->pars.argv_size[j] + 1));
+		d->cmds[j].args = malloc(sizeof(char *) * (d->pars.argv_size[j] + 2));
 		cmds[j].args = d->cmds[j].args;
 		ft_memset(cmds[j].args, 0, sizeof(cmds[j].args));
 		cmds[j].id = j;
