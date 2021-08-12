4f57bcaf97136f5e7ca3cd137999728e0c3fac8c
Florian CAVILLON <fcavillo@e1r7p18.42.fr>
2021-08-12 16:30:18 +0200

    pwd changed args

--- srcs/execute/execute.c
+++ srcs/execute/execute.c
@@ -6,20 +6,18 @@
 /*   By: fcavillo <fcavillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/12 12:27:17 by mvaldes           #+#    #+#             */
-/*   Updated: 2021/08/12 16:31:33 by fcavillo         ###   ########.fr       */
+/*   Updated: 2021/08/12 16:32:22 by fcavillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execute.h"
 #include "../minishell.h"
 
-// returns 0 if there is a problem -> error handling
-// 
-
 int	execute(t_data *data)
 {
 	int			i;
 	t_commands	cmd;
+	extern char **environ;
 
 	i = 0;
 	while (i < data->pars.cmd_nbr)
--- srcs/execute/execute.h
+++ srcs/execute/execute.h
@@ -7,22 +7,35 @@
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 18:08:59 by mvaldes           #+#    #+#             */
 <<<<<<< HEAD
+<<<<<<< HEAD
 /*   Updated: 2021/08/12 16:38:19 by fcavillo         ###   ########.fr       */
 =======
+=======
+>>>>>>> pwd changed args
 <<<<<<< HEAD
 /*   Updated: 2021/08/12 14:25:30 by mvaldes          ###   ########.fr       */
 =======
 /*   Updated: 2021/08/12 12:36:30 by fcavillo         ###   ########.fr       */
 >>>>>>> pwd
+<<<<<<< HEAD
 >>>>>>> pwd
+=======
+=======
+/*   Updated: 2021/08/12 14:24:27 by fcavillo         ###   ########.fr       */
+>>>>>>> pwd changed args
+>>>>>>> pwd changed args
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXECUTE_H
 # define EXECUTE_H
 
+<<<<<<< HEAD
 #include "../minishell.h"
 
+=======
+<<<<<<< HEAD
+>>>>>>> pwd changed args
 int	builtin_cd(char **args);
 int	builtin_echo(char **args);
 int	builtin_env(char **args);
@@ -30,5 +43,16 @@ int	builtin_exit(char **args);
 int	builtin_export(char **args);
 int	builtin_pwd(char **args);
 int	builtin_unset(char **args);
+=======
+#include "../minishell.h"
+
+void	builtin_cd(char **args);
+void	builtin_echo(char **args);
+void	builtin_env(char **args);
+void	builtin_exit(char **args);
+void	builtin_export(char **args);
+void	builtin_pwd(char **args);
+void	builtin_unset(char **args);
+>>>>>>> pwd changed args
 
 #endif
