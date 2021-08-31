c3d146b07b6858c772c3547da502e0ebf6559eef
Marie-lou VALDES <mvaldes@e1r7p16.42.fr>
2021-08-31 15:05:24 +0200

    unset rework done

--- srcs/execute/cmd_builtin/unset.c
+++ srcs/execute/cmd_builtin/unset.c
@@ -6,54 +6,65 @@
 /*   By: mvaldes <mvaldes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 18:43:25 by mvaldes           #+#    #+#             */
-/*   Updated: 2021/08/31 13:03:58 by mvaldes          ###   ########.fr       */
+/*   Updated: 2021/08/31 15:05:05 by mvaldes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../execute.h"
 
-static void	shift_all_env_var(char	***environ_var, int i, int size)
+static int	shift_all_env_var(char	***env_var, char **tmp_env, int i, int size)
 {
-	char		**tmp_env;
-
-	tmp_env = malloc(sizeof(char *) * size);
-	while ((*environ_var)[i])
+	ft_free_str(&tmp_env[i]);
+	while (i < size - 1)
 	{
-		free((*environ_var)[i]);
-		(*environ_var)[i] = NULL;
-		if ((*environ_var)[i + 1] != NULL)
-			(*environ_var)[i] = ft_strdup((*environ_var)[i + 1]);
-		else
-			break ;
+		(*env_var)[i] = ft_strdup(tmp_env[i + 1]);
+		ft_free_str(&tmp_env[i + 1]);
 		i++;
 	}
+	(*env_var)[i] = NULL;
+	return (i);
+}
+
+static void	init_unset(char ***environ_var, char ***tmp_env, int *size)
+{
+	int		i;
+
+	i = -1;
+	(*size) = -1;
+	while ((*environ_var)[++(*size)])
+		;
+	(*tmp_env) = malloc(sizeof(char *) * (*size + 1));
+	while ((*environ_var)[++i])
+	{
+		(*tmp_env)[i] = ft_strdup((*environ_var)[i]);
+		ft_free_str(&(*environ_var)[i]);
+	}
+	ft_free_str(*environ_var);
+	(*environ_var) = malloc(sizeof(char *) * *size);
 }
 
 int	builtin_unset(char **args, char ***environ_var)
 {
 	int			i;
 	char		**split_env;
-	int			found;
 	int			size;
+	char		**tmp_env;
 
-	found = 0;
-	size = -1;
-	while ((*environ_var)[++size])
-		;
+	init_unset(environ_var, &tmp_env, &size);
 	i = -1;
-	while ((*environ_var)[++i])
+	while (i < size - 1 && tmp_env[++i])
 	{
-		split_env = ft_split((*environ_var)[i], '=');
+		split_env = ft_split(tmp_env[i], '=');
 		if (ft_strncmp(args[1], split_env[0], \
 		ft_strlen(split_env[0])) == 0)
 		{
-			found = 1;
-			shift_all_env_var(environ_var, i, size);
-			(*environ_var)[i] = NULL;
+			i = shift_all_env_var(environ_var, tmp_env, i, size);
+			free_split(split_env);
+			return (1);
 		}
+		(*environ_var)[i] = ft_strdup(tmp_env[i]);
+		ft_free_str(&tmp_env[i]);
 		free_split(split_env);
 	}
-	if (!found)
-		return (0);
-	return (1);
+	return (0);
 }
