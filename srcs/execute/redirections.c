7d8ac2b2f7f2ae41fefde462af3ab6e4231a8849
Florian CAVILLON <fcavillo@e1r7p18.42.fr>
2021-09-02 15:28:42 +0200

    started redirs

--- srcs/execute/redirections.c
+++ srcs/execute/redirections.c
@@ -6,7 +6,7 @@
 /*   By: fcavillo <fcavillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/02 14:28:09 by fcavillo          #+#    #+#             */
-/*   Updated: 2021/09/02 15:11:39 by fcavillo         ###   ########.fr       */
+/*   Updated: 2021/09/02 15:24:20 by fcavillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 /*
 int check_redir(t_data *data)
 {
-    
+	
 }
 
 int simple_redir(t_data *data, int i)
 {
-    int fd;
-
-    
-}*/
+	int		fd;
+	char	**redir;
+	
+	
+}
+*/
