76f2cc9a61f9377383d70ce2536ee9431199b168
mvaldes <mvaldes@student.42.fr>
2021-09-03 14:14:32 +0200

    changed token split

--- srcs/parsing/p_utils/token_split_utils.c
+++ srcs/parsing/p_utils/token_split_utils.c
@@ -6,7 +6,7 @@
 /*   By: mvaldes <mvaldes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/24 15:23:03 by mvaldes           #+#    #+#             */
-/*   Updated: 2021/09/02 17:18:10 by mvaldes          ###   ########.fr       */
+/*   Updated: 2021/09/03 14:11:26 by mvaldes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,26 @@
 
 static void	regular_word(t_split *s, const char *str)
 {
-	while (str[s->end] && str[s->end] != SPACE && str[s->end] != TAB && \
-	str[s->end] != PIPE_C && str[s->end] != R_IN && str[s->end] != R_OUT)
+	int		inside_quote_s;
+	int		inside_quote_d;
+
+	inside_quote_s = 0;
+	inside_quote_d = 0;
+	while (str[s->end] && str[s->end] != TAB && str[s->end] != PIPE_C \
+	&& str[s->end] != R_IN && str[s->end] != R_OUT)
+	{
+		if (str[s->end] == SPACE && (!inside_quote_s && !inside_quote_d))
+			break ;
+		else if (str[s->end] == S_QUOTE && !inside_quote_s)
+			inside_quote_s = 1;
+		else if (str[s->end] == S_QUOTE && inside_quote_s)
+			inside_quote_s = 0;
+		else if (str[s->end] == D_QUOTE && !inside_quote_d)
+			inside_quote_d = 1;
+		else if (str[s->end] == D_QUOTE && inside_quote_d)
+			inside_quote_d = 0;
 		s->end++;
+	}
 	s->dest[s->i] = ft_substr(str, s->start, (s->end - s->start));
 	s->start = s->end;
 	s->i++;
@@ -80,11 +97,11 @@ void	special_split_2(t_split *s, const char *str)
 		if (str[s->end] && str[s->end] != SPACE && str[s->end] != TAB && \
 		str[s->end] != PIPE_C && str[s->end] != R_IN && str[s->end] != R_OUT)
 			regular_word(s, str);
-		// if (str[s->end] && (str[s->end] == S_QUOTE || str[s->end] == D_QUOTE))
-		// 	quote_word(s, str);
 		else if (str[s->end] && (str[s->end] == PIPE_C))
 			spe_word(s, str);
 		else if (str[s->end] && (str[s->end] == R_IN || str[s->end] == R_OUT))
 			redir_word(s, str);
 	}
 }
+// if (str[s->end] && (str[s->end] == S_QUOTE || str[s->end] == D_QUOTE))
+// 	quote_word(s, str);
